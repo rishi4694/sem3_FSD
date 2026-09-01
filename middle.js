7b28af2d856300fee76b992f17005c04911ad467
Soyanshu Khuntia <soyanshukhuntia@gmail.com>
2026-09-01 16:19:05 +0530

    Add files via upload

---
+++ middle.js
@@ -0,0 +1,22 @@
+// middle level javascript code
+// global scope
+// let collage="abes";
+// function display(){
+//     console.log(collage);
+// }
+// display();
+// console.log(collage);
+
+// function scope
+// function display1(){
+//     let collage="abesec";
+//     console.log(collage);
+// }
+// display1();
+// console.log(collage);
+
+// block scope
+if(true){
+    let collage="abesec";
+    console.log(collage);
+}
